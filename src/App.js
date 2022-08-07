2f5f092ec69f6323b0ed7adece1e9bb7ad453b95
Anmol <anmoldhage999@gmail.com>
2022-08-07 10:39:35 +0530

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
 import './App.css';
+import Navbar from './component/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Plants from './pages/Plants/Plants';
 
 function App() {
   return (
     <div>
-      react
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign-in" element={<Plants />} />
+          <Route path="/sign-up" element={<Plants />} />
+          <Route element={<Navbar />}>
+            <Route path="/" element={<Plants />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
